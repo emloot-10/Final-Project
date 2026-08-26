477963174ac6ff587f7d34dd165b9f5ed12d41fb
Emily Lootens <ealoo10s@gmail.com>
2026-08-26 15:55:29 -0400

    more updates

--- New Data.R
+++ New Data.R
@@ -18,57 +18,51 @@ cytomeg <- medicaldata::cytomegalovirus
 ###THE FILE PATHS AT LEAST TWICE"
 
 #saving data to computer with here::
-getwd()
-setwd("data")
+#getwd()
+#setwd("data")
 
-if (!dir.exists(here::here("data", "project", "data.csv"))) {
-	dir.create(here::here("data", "project", "data.csv"))
-}
+#if (!dir.exists(here::here("data", "project", "data.csv"))) {
+#	dir.create(here::here("data", "project", "data.csv"))
+#}
 
 
 
-write_rds(cytomeg, here::here("data", "clean", "cytomegalovirus.rds"))
+#write_rds(cytomeg, here::here("data", "clean", "cytomegalovirus.rds"))
 
 #here::here again?
-cyto <- read_csv(here::here("data", "raw", "cytomegalovirus.rds"),
-								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
-								 skip = 1, col_names = cytomeg_cols)
-
-###"WRITE AND USE A FUNCTION WITH THE DATA"###
-#creating a function to turn variables into character variables
-cytomeg$prior.radiation <- as.character(cytomeg$prior.radiation)
-
-characterize <- function(data, variable){
-	char <- as.character(data[[variable]])
-	return(char)
-}
+#cyto <- read_csv(here::here("data", "raw", "cytomegalovirus.rds"),
+#								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+#								 skip = 1, col_names = cytomeg_cols)
 
 
-class(cytomeg$prior.transplant)
-characterize(cytomeg, "prior.transplant")
-
-
-cytomeg$diagnosis.type <- as.character(cytomeg$diagnosis.type)
-cytomeg$cmv<-as.character(cytomeg$cmv)
 
 
 ###CREATE A GTSUMMARY TABLE WITH DESCRIPTIVE STATISTICS"###
 
 #sex data dictionary: 1 is male, 0 is female
 #diagnosis: 1 is myeloid, 0 is lymphoid
+#first: need to make the variables of interest character vars not numeric
+cytomeg$diagnosis.type <- as.character(cytomeg$diagnosis.type)
+cytomeg$cmv<-as.character(cytomeg$cmv)
+cytomeg$prior.transplant <- as.character(cytomeg$prior.transplant)
+cytomeg$sex <- as.character(cytomeg$sex)
+cytomeg$prior.radiation <- as.character(cytomeg$prior.radiation)
+
+
 
 tbl_summary(
 	cytomeg,
 	by = sex,
 	include = c(
-		sex, diagnosis.type, prior.radiation, prior.chemo, cmv, time.to.cmv
+		sex, diagnosis.type, prior.radiation, prior.chemo, prior.transplant, cmv, time.to.cmv
 	),
 	label = list(
 		diagnosis.type ~ "Type of Diagnosis (0 = lymphoid, 1 = myeloid)",
 		prior.radiation ~ "Prior radiation (0 = no, 1 = yes)",
 		time.to.cmv ~ "Time to CMV reactivation (in months)",
 		cmv ~ "Cytomegalovirus reactivation posttransplant (0 = no, 1 = yes)",
-		prior.chemo ~ "Number of prior chemotherapy regimens"
+		prior.chemo ~ "Number of prior chemotherapy regimens",
+		prior.transplant ~ "Prior transplant (0 = no, 1 = yes)"
 	),   missing_text = "Missing"
 ) |>
 	# change the test used to compare sex_cat groups
@@ -79,11 +73,19 @@ tbl_summary(
 	# add a total column with the number of observations
 	add_overall(col_label = "**Total** N = {N}") |>
 	bold_labels()
-#this doesn't work well because it doesn't really portray the diagnosis data since it's dichotomous :/
+
 
 
 
 ###"CREATE A FIGURE"###
 
 hist(cytomeg$time.to.cmv, main = "Time to CMV Reactivation", xlab = "Months",
-		 col = "purple")
+		 col = "navy")
+
+
+###"WRITE AND USE A FUNCTION WITH THE DATA"###
+#function to turn 0's into no and 1's to yes for certain variables
+
+y_n <- function(data, variable){
+
+}
