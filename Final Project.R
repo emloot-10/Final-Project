827ed1a5c055361853d27a3836a7e795194ffc3a
Emily Lootens <ealoo10s@gmail.com>
2026-08-31 17:37:22 -0400

    Final Final Commit, changed doc name

---
+++ Final Project.R
@@ -0,0 +1,112 @@
+#installing and loading required packages and datasets
+
+install.packages("medicaldata")
+install.packages("remotes")
+install.packages("here")
+install.packages("readr")
+remotes::install_github("higgi13425/medicaldata")
+library(medicaldata)
+library(readr)
+library(gtsummary)
+data(package = "medicaldata")
+
+cytomeg <- medicaldata::cytomegalovirus
+
+
+
+###"READ IN A DATASET AND SAVE A FILE USING THE HERE PACKAGE AND REFER TO###
+###THE FILE PATHS AT LEAST TWICE"
+
+#saving data to computer with here::
+getwd()
+setwd("data")
+
+if (!dir.exists(here::here("data", "project", "data.csv"))) {
+	dir.create(here::here("data", "project", "data.csv"))
+}
+
+write_rds(cytomeg, here::here("data", "clean", "cytomegalovirus.rds"))
+
+
+###"WRITE AND USE A FUNCTION WITH THE DATA"###
+#function to turn 0's into no and 1's to yes for certain variables
+
+y_n <- function(data){
+	yn<-ifelse(data == 0, "no",
+				 ifelse(data == 1, "yes", NA))
+}
+
+#creating a new column in the df to make it a character variable
+
+cytomeg$prior.radiation.char<-y_n(cytomeg$prior.radiation)
+cytomeg$prior.transplant.char<-y_n(cytomeg$prior.transplant)
+cytomeg$cmv.char<-y_n(cytomeg$cmv)
+
+###CREATE A GTSUMMARY TABLE WITH DESCRIPTIVE STATISTICS"###
+
+#sex data dictionary: 1 is male, 0 is female
+#diagnosis: 1 is myeloid, 0 is lymphoid
+#first: need to make the variables of interest character vars not numeric
+cytomeg$diagnosis.type <- as.character(cytomeg$diagnosis.type)
+cytomeg$cmv<-as.character(cytomeg$cmv)
+cytomeg$prior.transplant <- as.character(cytomeg$prior.transplant)
+cytomeg$sex <- as.character(cytomeg$sex)
+cytomeg$prior.radiation <- as.character(cytomeg$prior.radiation)
+
+
+
+desc_stats <- tbl_summary(
+	cytomeg,
+	by = sex,
+	include = c(
+		sex, diagnosis.type, prior.radiation, prior.chemo, prior.transplant, cmv, time.to.cmv
+	),
+	label = list(
+		diagnosis.type ~ "Type of Diagnosis (0 = lymphoid, 1 = myeloid)",
+		prior.radiation ~ "Prior radiation (0 = no, 1 = yes)",
+		time.to.cmv ~ "Time to CMV reactivation (in months)",
+		cmv ~ "Cytomegalovirus reactivation posttransplant (0 = no, 1 = yes)",
+		prior.chemo ~ "Number of prior chemotherapy regimens",
+		prior.transplant ~ "Prior transplant (0 = no, 1 = yes)"
+	),   missing_text = "Missing"
+) |>
+	add_p(test = list(
+		all_continuous() ~ "t.test",
+		all_categorical() ~ "chisq.test"
+	)) |>
+	add_overall(col_label = "**Total** N = {N}") |>
+	bold_labels()
+
+
+
+#using the new character variables in the descriptive stats:
+
+char_desc_stats <- tbl_summary(
+	cytomeg,
+	by = sex,
+	include = c(
+		sex, diagnosis.type, prior.radiation.char, prior.chemo, prior.transplant.char, cmv.char, time.to.cmv
+	),
+	label = list(
+		diagnosis.type ~ "Type of Diagnosis (0 = lymphoid, 1 = myeloid)",
+		prior.radiation.char ~ "Had prior radiation treatment",
+		time.to.cmv ~ "Time to CMV reactivation (in months)",
+		cmv.char ~ "Had cytomegalovirus reactivation posttransplant",
+		prior.chemo ~ "Number of prior chemotherapy regimens",
+		prior.transplant.char ~ "Had a prior transplant"
+	),   missing_text = "Missing"
+) |>
+	add_p(test = list(
+		all_continuous() ~ "t.test",
+		all_categorical() ~ "chisq.test"
+	)) |>
+	add_overall(col_label = "**Total** N = {N}") |>
+	bold_labels()
+
+
+
+###"CREATE A FIGURE"###
+
+hist(cytomeg$time.to.cmv, main = "Time to CMV Reactivation", xlab = "Months",
+		 col = "navy")
+
